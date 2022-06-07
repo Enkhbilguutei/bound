cda91bcf31de932f171c155eaed137b4fbbb61b2
Vincwithcobain <shijir.batbaatar0227@gmail.com>
2022-06-07 15:14:29 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-import Header from "./component/Header";
+import Header from "./component/common/Header";
+import Banner from "./component/Banner";
+import Intro from "./component/Intro";
+import Card from "./component/Card";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
+      <Intro />
+      <Card />
     </div>
   );
 }
